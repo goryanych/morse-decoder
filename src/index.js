f5a63a62270240f29c7adaeb5f73f90eb31e5a9f
Ihor Kolohryvyi <Kolohryvyi@gmail.com>
2022-01-28 14:46:30 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const strToArray = [];
+    const arrayToMorse = [];
+    let startIndex = 0;
+
+    for (let i = 1; i <= expr.length; i++) {
+        if (i % 10 === 0) {
+            strToArray.push(expr.slice(startIndex, i));
+            startIndex = i;
+        }
+    }
+
+    for (let elem of strToArray) {
+        let str = '';
+
+        for (let i = 0; i < elem.length; i++) {
+            if (elem[i] == 1) {
+                if (elem[++i] == 0) str += '.';
+                else str += '-'
+            }
+        }
+        arrayToMorse.push(str);
+    }
+
+    const result = arrayToMorse.reduce((acc, e) => {
+        if (!MORSE_TABLE[e]) acc += ' ';
+        else acc += MORSE_TABLE[e];
+
+        return acc;
+
+    }, '');
+    
+    return result;
 }
 
 module.exports = {
